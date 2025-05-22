98a298ee06731aa1925bc02ae910de3de9873eb8
Vicentesan <vikom.sanchez@gmail.com>
2025-05-22 00:51:11 -0300

    feat: add db schema

--- packages/db/src/schema/waitlist.ts
+++ packages/db/src/schema/waitlist.ts
@@ -1,1 +1,12 @@
-export const 
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+
+export const waitlist = pgTable("waitlist", {
+  id: uuid().primaryKey().defaultRandom(),
+  email: text().notNull().unique(),
+  joinedAt: timestamp({
+    mode: "date",
+    withTimezone: true,
+  })
+    .notNull()
+    .defaultNow(),
+});
