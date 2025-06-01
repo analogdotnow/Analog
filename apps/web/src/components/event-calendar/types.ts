00c1f54b0a425f77a851f7c4489b6f074a482f56
iboughtbed <testpythontosanzhar@gmail.com>
2025-06-01 16:14:57 +0500

    chore: remove comments

--- apps/web/src/components/event-calendar/types.ts
+++ apps/web/src/components/event-calendar/types.ts
@@ -4,19 +4,6 @@ export type CalendarView = "month" | "week" | "day" | "agenda";
 
 export type CalendarEvent = RouterOutputs["events"]["list"]["events"][number];
 
-// export interface CalendarEvent {
-//   id: string;
-//   title: string;
-//   description?: string;
-//   start: Date;
-//   end: Date;
-//   allDay?: boolean;
-//   color?: EventColor;
-//   location?: string;
-//   calendarId: string;
-//   connectionId: string;
-// }
-
 export type EventColor =
   | "sky"
   | "amber"
