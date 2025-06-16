5dfcab194401f059aefb639ca2a7be53b9e7680b
Artem Popelyshev <apopelyshev@gmail.com>
2025-06-16 04:43:32 +0200

    bug: simpleanalytics code is dumb

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 import withSimpleAnalytics from "@simpleanalytics/next/plugin";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_SIMPLE_ANALYTICS_HOSTNAME:
+      process.env.NEXT_PUBLIC_SIMPLE_ANALYTICS_HOSTNAME ?? "localhost",
+  },
 };
 
 export default withSimpleAnalytics(nextConfig);
