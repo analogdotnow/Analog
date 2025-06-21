93f6fbb0d4d541c313a9590bed70d96f12fafa2e
iboughtbed <testpythontosanzhar@gmail.com>
2025-06-21 23:52:02 +0500

    feat: calendar settings

--- packages/api/src/routers/calendars.ts
+++ packages/api/src/routers/calendars.ts
@@ -1,4 +1,13 @@
-import { calendarProcedure, createTRPCRouter } from "../trpc";
+import { eq } from "drizzle-orm";
+import { z } from "zod";
+
+import { user } from "@repo/db/schema";
+
+import {
+  calendarProcedure,
+  createTRPCRouter,
+  protectedProcedure,
+} from "../trpc";
 
 export const calendarsRouter = createTRPCRouter({
   list: calendarProcedure.query(async ({ ctx }) => {
@@ -16,7 +25,39 @@ export const calendarsRouter = createTRPCRouter({
     const accounts = await Promise.all(promises);
 
     return {
+      defaultCalendarId: ctx.user.defaultCalendarId,
+      defaultAccountId: ctx.user.defaultAccountId,
       accounts,
     };
   }),
+
+  getDefaultId: protectedProcedure.query(async ({ ctx }) => {
+    // We would need to remove this later probably
+    // Because we can just get the default calendar from the better auth session
+
+    const foundUser = await ctx.db.query.user.findFirst({
+      where: (table, { eq }) => eq(table.id, ctx.user.id),
+    });
+
+    return {
+      defaultCalendarId: foundUser?.defaultCalendarId ?? null,
+    };
+  }),
+
+  setDefaultId: protectedProcedure
+    .input(
+      z.object({
+        calendarId: z.string(),
+        accountId: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db
+        .update(user)
+        .set({
+          defaultCalendarId: input.calendarId,
+          defaultAccountId: input.accountId,
+        })
+        .where(eq(user.id, ctx.user.id));
+    }),
 });
