2b3881badd2ec42aa83ef8703419fab9989fe630
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2025-06-24 09:38:58 -0700

    Fix build fail

--- apps/web/src/lib/constants.ts
+++ apps/web/src/lib/constants.ts
@@ -6,6 +6,11 @@ export const providers = [
     icon: Google,
     providerId: "google" as const,
   },
+  {
+    name: "Outlook",
+    icon: Microsoft,
+    providerId: "microsoft" as const,
+  },
 ];
 
 export type ProviderId = (typeof providers)[number]["providerId"];
