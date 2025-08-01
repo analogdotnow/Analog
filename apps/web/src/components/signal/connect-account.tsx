3e463a617ab2dc3d89fc2b24976d528a669d2542
Jean P.D. Meijer <jean@analoginterface.io>
2025-08-01 13:40:18 +0200

    wip

--- apps/web/src/components/signal/connect-account.tsx
+++ apps/web/src/components/signal/connect-account.tsx
@@ -1,14 +1,26 @@
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import { authClient } from "@repo/auth/client";
+import { useMutation } from "@tanstack/react-query";
 
 interface ConnectAccountProps {
   className?: string;
 }
 
 export function ConnectAccount({ className }: ConnectAccountProps) {
+  const mutation = useMutation({
+    mutationFn: async () => {
+      const response = await authClient.signIn.oauth2({
+        providerId: "zero",
+        callbackURL: "/calendar",
+      });
+      return response;
+    },
+  });
+
   return (
     <div className={cn("flex flex-col gap-2", className)}>
-      <Button>Connect Account</Button>
+      <Button onClick={() => mutation.mutate()}>Connect Account</Button>
     </div>
   );
 }
