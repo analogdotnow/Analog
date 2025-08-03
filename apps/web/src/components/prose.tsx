4a2f07dd4c80ea6e42616e2d7589296c08b9f84a
Dominik Koch <dominik@koch-bautechnik.de>
2025-08-03 23:48:28 +0200

    fix: use interface over type

--- apps/web/src/components/prose.tsx
+++ apps/web/src/components/prose.tsx
@@ -2,7 +2,7 @@ import type React from "react";
 
 import { cn } from "@/lib/utils";
 
-type ProseProps = React.HTMLAttributes<HTMLElement> & {
+interface ProseProps extends React.HTMLAttributes<HTMLElement> {
   as?: "article";
   html: string;
 };
