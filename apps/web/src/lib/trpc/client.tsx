78c4879f0f269f01bcc650c095c69f3567948ed2
Jean P.D. Meijer <jean@analoginterface.io>
2026-03-25 01:31:43 +0100

    wip

--- apps/web/src/lib/trpc/client.tsx
+++ apps/web/src/lib/trpc/client.tsx
@@ -48,7 +48,7 @@ export const trpc = createTRPCClient<AppRouter>({
   links: [
     loggerLink({
       enabled: (op) =>
-        process.env.NEXT_PUBLIC_ENV === "development" ||
+        env.NEXT_PUBLIC_ENV === "development" ||
         (op.direction === "down" && op.result instanceof Error),
     }),
     httpBatchStreamLink({
@@ -77,7 +77,7 @@ export function TRPCReactProvider(props: Readonly<TRPCReactProviderProps>) {
       links: [
         loggerLink({
           enabled: (op) =>
-            process.env.NEXT_PUBLIC_ENV === "development" ||
+            env.NEXT_PUBLIC_ENV === "development" ||
             (op.direction === "down" && op.result instanceof Error),
         }),
         httpBatchStreamLink({
